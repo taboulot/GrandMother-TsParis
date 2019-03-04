95b9438bc969029fc4711b1d400a4192ca0b99a6
Clément <clement-taboulot@hotmail.fr>
2019-03-04 13:31:01 +0100

    Component without type

---
+++ src/GrandMother.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Component } from "react";
+import { Text, View } from "react-native";
+
+class GrandMother extends Component {
+  public render() {
+    return (
+      <View>
+        <Text>Grand Mother</Text>
+      </View>
+    );
+  }
+}
+
+export default GrandMother;
